cc0aef5e8bd152e0c733066edcccc423449606f2
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2025-11-17 17:22:27 +0600

    problem 1-8 solved

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Assignment_One
+
+1. What are some differences between interfaces and types in TypeScript?
+
+2. What is the use of the keyof keyword in TypeScript? Provide an 3. example.
+
+3. Explain the difference between any, unknown, and never types in TypeScript.
+
+4. What is the use of enums in TypeScript? Provide an example of a numeric and string enum.
+
+5. Provide an example of using union and intersection types in TypeScript.
--- solution.ts
+++ solution.ts
@@ -8,6 +8,9 @@ const formatValue = (value: string | number | boolean) => {
   }
 };
 
+
+
+
 const getLength = (value: string | (string[] | number[])) => {
   if (typeof value === "string") {
     return value.length as number;
@@ -15,6 +18,9 @@ const getLength = (value: string | (string[] | number[])) => {
     return value.length as number;
   }
 };
+
+
+
 
 class Person {
   name: string;
@@ -28,6 +34,9 @@ class Person {
   }
 }
 
+
+
+
 const filterByRating = (
   value: {
     title: string;
@@ -36,6 +45,9 @@ const filterByRating = (
 ) => {
   return value.filter((item) => item["rating"] >= 4);
 };
+
+
+
 
 const filterActiveUsers = (
   value: {
@@ -47,6 +59,9 @@ const filterActiveUsers = (
 ) => {
   return value.filter((item) => item["isActive"] === true);
 };
+
+
+
 
 interface Book {
   title: string;
@@ -62,9 +77,55 @@ const printBookDetails = (book: Book) => {
   );
 };
 
+
+
+
 const getUniqueValues = (
   value1: number[] | string[],
-  value2: string[] | string[]
+  value2: number[] | string[]
 ) => {
-    //Todo Build your logic...
+  const result = [];
+  const checkDuplicate: Record<string | number, boolean> = {};
+
+  for (let i = 0; i < value1.length; i++) {
+    const item = value1[i];
+
+    if (!checkDuplicate[item]) {
+      result[result.length] = item;
+      checkDuplicate[item] = true;
+    }
+  }
+
+  for (let i = 0; i < value2.length; i++) {
+    const item = value2[i];
+
+    if (!checkDuplicate[item]) {
+      result[result.length] = item;
+      checkDuplicate[item] = true;
+    }
+  }
+  return result as [];
+};
+
+
+
+
+const calculateTotalPrice = (
+  value: {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+  }[]
+) => {
+  const totalPrice = value.reduce((total, item) => {
+    const subtotal = item.price * item.quantity;
+    const discountPrice = item.discount
+      ? subtotal * (1 - item.discount / 100)
+      : subtotal;
+
+    return total + discountPrice;
+  },0);
+
+  return totalPrice as number;
 };
